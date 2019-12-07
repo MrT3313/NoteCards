e452ded1b5484026ccf64ecc5189a1f3968bc9c8
Mr.T <rturgeon13@gmail.com>
2019-12-07 16:01:38 -0600

    Updated: Project file structure
    broke Views our of Pages

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -1,19 +1,23 @@
-// implement Gatsby’s Node.js APIs to customize and extend default settings affecting the build process
-// Creates Pages Dynamically
+/* - MAIN - 
+    - Links -
+        - https://www.gatsbyjs.org/docs/node-apis/
+    - Docs -
+        - implement Gatsby’s Node.js APIs to customize and extend default settings affecting the build process
+    - Notes -
+        - Creates Pages Dynamically
+*/
 
-// -- //
-
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
-
-
-// Implement the Gatsby API “onCreatePage”. This is
-// called after every page is created.
+// - 1 - //
+/* - Links -
+    Video Link: https://www.youtube.com/watch?list=PLz8Iz-Fnk_eTpvd49Sa77NiF8Uqq5Iykx&v=j-vuF2PYHmU&feature=emb_title
+*/
+/* - Docs - 
+    Implement the Gatsby API “onCreatePage”. This is
+    called after every page is created.
+*/
+/* - Notes -
+    setting up dynamic routes off of /app/... path
+*/
 exports.onCreatePage = async ({ page, actions }) => {
     const { createPage } = actions
     // page.matchPath is a special key that's used for matching pages
@@ -22,5 +26,26 @@ exports.onCreatePage = async ({ page, actions }) => {
         page.matchPath = "/app/*"
         // Update the page.
         createPage(page)
+    }
+}
+
+
+// - 2 - //
+/* - Links -
+    VideoLink: https://youtu.be/j-vuF2PYHmU?list=PLz8Iz-Fnk_eTpvd49Sa77NiF8Uqq5Iykx&t=1246
+*/
+/* - Notes -
+    Prevents Auth0 from doing something sometime...
+*/
+exports.onCreateWebpackConfig = ({ stage, loaders, actions}) => {
+    if (stage === 'build-html') {
+        actions.setSetpackConfig({
+            module: {
+                rules: {
+                    test: /auth0-spa-js/,
+                    use: loaders.null()
+                }
+            }
+        })
     }
 }
--- client/src/pages/app.js
+++ client/src/pages/app.js
@@ -3,8 +3,8 @@ import React from 'react'
 import { Router } from "@reach/router"
 
 // COMPONENTS
-import HomePage from './HomePage.js'
-// import Login from '../pages/Login.js'
+import HomePage from '../Views/HomePage.js'
+import Login from '../Views/Login.js'
 
 const HomePage_Test = () => <p>HomePage_Test</p> 
 const AppHome = () => <p>AppHome</p>
@@ -14,8 +14,9 @@ const App = () => (
         <Router>
             {/* <HomePage_Test path='/app/HomePage'/> */}
             <HomePage path='/app/HomePage'/>
+            <Login path='/app/Login'/>
+
             <AppHome path='/app' />
-            
         </Router>
 )
 
