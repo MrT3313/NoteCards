b330dded2c0162c216c7b4d1bfc82a2831b4606b
Mr.T <rturgeon13@gmail.com>
2019-12-19 12:28:29 -0500

    netlify working -- setting up auth0

---
+++ client/src/utils/Auth0_Client.js
@@ -0,0 +1,9 @@
+// IMPORTS
+import createAuth0Client from '@auth0/auth0-spa-js';
+
+// Create Auth0
+const auth0 = await createAuth0Client({
+    // domain: 
+    // client_id: 
+    // redirect_uri: 
+})
