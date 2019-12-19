e28d87959b6bee90481b10c21da27a839adbdc27
Mr.T <rturgeon13@gmail.com>
2019-12-19 12:09:37 -0500

    initial commit

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,81 +1,9 @@
-// configure options for a Gatsby site, with metadata for project title, description, plugins, etc.
-
-// -- //
-
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
-});
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
 
 module.exports = {
-  
-  // METADATA
-  siteMetadata: {
-    // title: `Gatsby Default Starter`,
-    title: `NoteCards`,
-    subTitle: `The Proving Ground`,
-    description: `Description`,
-    author: `Reed Turgeon`,
-  },
-
-  // PLUGINS
-  plugins: [
-    // - 1 - // ACCESS: Markdown Data Plugin
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: 'src',
-        path: `${__dirname}/src/`
-      }
-    },
-    // - 1.1 - // TRANSFORM: Markdown File into usefull html
-    `gatsby-transformer-remark`,
-
-    // - 2 - // Contentful CMS Plugin
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        // Learn about environment variables: https://gatsby.dev/env-vars
-
-        spaceId: process.env.CONTENTFUL_spaceID,
-        accessToken: process.env.CONTENTFUL_accessToken,
-      },
-    },
-
-    // - 3 - // SASS Plugin
-    `gatsby-plugin-sass`,
-
-    // - 4 - // Protecting HTML requests
-    `gatsby-plugin-react-helmet`,
-
-    // - 5 - // Using SVGs
-    {
-      resolve: `gatsby-plugin-react-svg`,
-      options: {
-        rule: {
-          include: /assets\/.*\.svg$/,
-        },
-      },
-    }
-
-
-    // Images
-    // `gatsby-plugin-sharp`,
-    // `gatsby-transformer-sharp`,
-
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+  /* Your site config here */
 }
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,37 +1,3 @@
-// IMPORTS
 import React from "react"
-import { useStaticQuery } from 'gatsby'
 
-// COMPONENTS
-import LandingPage from '../app/views/LandingPage.js'
-
-// MAIN COMPONENT TO EXPORT
-const GATSBY_INDEX = () => {
-  // - A - // Data & Variables
-  const data = useStaticQuery(query)
-  console.log('MAIN APP DATA',data)
-
-  // - B - // Return
-  return (
-    <div>
-      <LandingPage 
-        siteMetadata={data.site.siteMetadata}
-      />
-    </div>
-  )
-}
-
-// - A.1 - // Component GraphQL Query
-const query = graphql`
-    query {
-      site {
-          siteMetadata {
-              title
-              subTitle
-          }
-      }
-    }
-`
-
-// EXPORT
-export default GATSBY_INDEX
+export default () => <div>Hello world!</div>
